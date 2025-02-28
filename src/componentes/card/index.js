113bbf7f8027661ca499a837d2c544d417be86e9
Julio César <julio06.sp@gmail.com>
2025-02-28 02:14:31 -0300

    atualizada descrição do card

--- src/componentes/card/index.js
+++ src/componentes/card/index.js
@@ -5,6 +5,7 @@ export const Card = () => {
         <div className="card">
         <div className="titulo-card">
             <h4>exemplo de titulo cartao</h4>
+            <h6>Exemplo de descrição lorem ipsun idolrojred sodoskdosk dkoskdosdisdhdsjhdisjisdjdis dkshjdshdks djsidjsijidsj djisdjisj idolrojred sodoskdosk dkoskdosdisdhdsjhdisjisdjdis dkshjdshdks djsidjsijidsj djisdjisj idolrojred sodoskdosk dkoskdosdisdhdsjhdisjisdjdis dkshjdshdks djsidjsijidsj djisdjisj</h6>
         </div>
       </div>
     )
